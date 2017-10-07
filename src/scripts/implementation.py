69b1eec71cbe9f7a3703652a97ea980fa2afdcfc
Sergei Volodin <etoestja@yandex.ru>
2017-10-07 18:28:20 +0200

    Add implementation.py with 4 functions and helpers from labs

---
+++ src/scripts/implementation.py
@@ -0,0 +1,28 @@
+import numpy as np
+from helpers import *
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma, debug = False):
+    losses, ws = gradient_descent(y, tx, initial_w, max_iters, gamma, debug)
+    return ws[-1]
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    losses, ws = stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma, debug = False)
+    return ws[-1]
+
+def least_squares(y, tx):
+    """calculate the least squares solution."""
+    # ***************************************************
+    # returns optimal weights
+    # ***************************************************
+
+    N = tx.shape[0]
+    w = np.linalg.pinv(tx.T @ tx) @ tx.T @ y
+    e = y - tx @ w
+    return w
+
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression."""
+    N = tx.shape[0]
+    w = np.linalg.pinv(tx.T @ tx + lambda_ * (2. * N) * np.eye(tx.shape[1])) @ tx.T @ y
+    e = y - tx @ w
+    return w
