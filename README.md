2ac324af70d8df69ca44787dc20055d03c7c5113
Sergei Volodin <etoestja@yandex.ru>
2017-10-30 15:28:11 +0100

    Add howto on run.py

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Project 1 for the EPFL ML course: https://mlo.epfl.ch/page-146520.html
 
 ## Structure
 * **data** train.csv and test.csv must be put to this folder
+* **src/run.py** is a file 
 * **src** contains jupyter notebooks
 * **src/scripts** contains python scripts (s.t. `implementations.py`)
 * **src/run_tests.py** tests critical functions
@@ -10,7 +11,26 @@ Project 1 for the EPFL ML course: https://mlo.epfl.ch/page-146520.html
 ## Installation
 This project requires Python3 and virtualenv
 ```
+ $ ls README.md
+# File should be in place
  $ virtualenv -p $(which python3) venv
  $ source venv/bin/activate
  $ pip install -r requirements.txt
+ $ ls data/test.csv data/train.csv
+# Files should be in place
+ $ cd src
+ $ python run_tests.py
+# Tests should pass
+ $ python run.py
+# File logreg_1_submission.csv should be generated after ~10min
 ```
+
+## What the run.py does
+1. Loading data
+2. Imputing missing features with mean values (taken w/o these missing values)
+3. Creating 'missing feature' feature
+4. Adding polynomial basis of degree 5
+4. Replacing categorical features with several binary ones
+5. Standardizing data (mean&std from train are used for test)
+6. Training Logistic Regression with lambda=1e-4 using Netwon's method gamma=1e-1, 80 iterations, fixed random seed
+7. Saving results to logreg_1_submission.csv
--- src/run.py
+++ src/run.py
@@ -9,6 +9,7 @@ output_path = 'logreg_1_submission.csv'
 deg = 5
 lambda_ = 1e-4
 gamma = 0.1
+max_iters = [50, 20]
 
 # Loading data
 print("Loading data")
@@ -19,20 +20,26 @@ print("Preprocessing data")
 X_p, (x_mean, x_std) = feature_processing.process_X(X, deg)
 X_t_p, _ = feature_processing.process_X(X_t, deg, (x_mean, x_std))
 
-#Logistic
+#Logistic regression
+
+# Converting -1,1 to 0,1
 y_01 = helpers.y_to_01(np.array([y]).T)
+
+# starting point: zero vector
 w0 = np.zeros((X_p.shape[1], 1))
 
+# These phases can be merged into one. They are kept this way to make the resulting file same as for kaggle
 print("Training 1")
 np.random.seed(42)
 w01, l = implementation.reg_logistic_regression_newton(y_01, X_p, lambda_ = lambda_,
-                                                      initial_w = w0, max_iters = 50, gamma = gamma,
+                                                      initial_w = w0, max_iters = max_iters[0], gamma = gamma,
                                                       debug = False)
 
 print("Training 2")
 np.random.seed(42)
 w010, l = implementation.reg_logistic_regression_newton(y_01, X_p, lambda_ = lambda_,
-                                                      initial_w = w01, max_iters = 20, gamma = gamma,
+                                                      initial_w = w01, max_iters = max_iters[1], gamma = gamma,
                                                       debug = False)
+# Saving results to submission file
 y_pred = proj1_helpers.predict_labels(w010, X_t_p)
 proj1_helpers.create_csv_submission(ids_t, y_pred, output_path)
